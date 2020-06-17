64df9154b3f7ad1c7df3be4661974a0e5aa74e49
Emil Kollstrøm <e.kollstrom@gmail.com>
2020-06-17 21:05:48 +0200

    Add fuel calculation

--- src/main/kotlin/no/kollstrom/aoc/One.kt
+++ src/main/kotlin/no/kollstrom/aoc/One.kt
@@ -1,11 +1,10 @@
 package no.kollstrom.aoc
 
+// https://adventofcode.com/2019/day/1
+
 const val DIVIDE_BY = 3
 const val SUBTRACT_BY = 2
 
 data class Module(val mass: Int) {
-    fun calculateRequiredFuel(): Int {
-        TODO("Not yet implemented")
-    }
-
+    fun calculateRequiredFuel(): Int = mass.div(DIVIDE_BY) - SUBTRACT_BY
 }

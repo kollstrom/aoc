4141ed2ca76cdc4861c8c6d8e768505412aeb100
Emil Kollstrøm <e.kollstrom@gmail.com>
2020-06-17 21:36:19 +0200

    Day 2: Add intcode calculator tests

---
+++ src/main/kotlin/no/kollstrom/aoc/Two.kt
@@ -0,0 +1,8 @@
+package no.kollstrom.aoc
+
+object IntCodeCalculator {
+    fun calculate(input: List<Int>): List<Int> {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/test/kotlin/no/kollstrom/aoc/IntCodeCalculatorTest.kt
@@ -0,0 +1,34 @@
+package no.kollstrom.aoc
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class IntCodeCalculatorTest {
+    @Test
+    internal fun `1,0,0,0,99 becomes 2,0,0,0,99`() {
+        val input = listOf(1, 0, 0, 0, 99)
+        val expected = listOf(2, 0, 0, 0, 99)
+        assertEquals(expected, IntCodeCalculator.calculate(input))
+    }
+
+    @Test
+    internal fun `2,3,0,3,99 becomes 2,3,0,6,99`() {
+        val input = listOf(2, 3, 0, 3, 99)
+        val expected = listOf(2, 3, 0, 6, 99)
+        assertEquals(expected, IntCodeCalculator.calculate(input))
+    }
+
+    @Test
+    internal fun `2,4,4,5,99,0 becomes 2,4,4,5,99,9801`() {
+        val input = listOf(2, 4, 4, 5, 99, 0)
+        val expected = listOf(2, 4, 4, 5, 99, 9801)
+        assertEquals(expected, IntCodeCalculator.calculate(input))
+    }
+
+    @Test
+    internal fun `1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99`() {
+        val input = listOf(1, 1, 1, 4, 99, 5, 6, 0, 99)
+        val expected = listOf(30, 1, 1, 4, 2, 5, 6, 0, 99)
+        assertEquals(expected, IntCodeCalculator.calculate(input))
+    }
+}
